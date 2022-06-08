96b5a1f29b73758d40d590c0ec74f4e0b0d2ef74
MY [D] TECH <105109129+MYDTECH@users.noreply.github.com>
2022-06-08 15:40:40 +0530

    Add files via upload

---
+++ D Assistant.py
@@ -0,0 +1,13 @@
+# main
+print(".....................................................................Welcome to D Assistant....................................................................")
+
+# name
+name = input("Enter your name : ")
+print("Hello! " + name)
+
+# help
+help = input("How can I help " + name + ':\n')
+
+# how are you
+if (help == "How are you?") :
+     print("I am fine")
